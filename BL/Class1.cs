940c313492595dca48ec1a9a10381333597cbe61
tzippykovalsky <9098537@gmail.com>
2024-05-23 21:57:50 +0300

    first

---
+++ BL/Class1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Class1
+    {
+        public User Login(string name, string password)
+        {
+
+            User user = new User();
+            try
+            {
+
+                using (EstyfridmanMathTestEntities db = new EstyfridmanMathTestEntities())
+                {
+                    user = db.User.FirstOrDefault(x => x.Name == name && x.Password == password);
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+            return user;
+
+        }
+
+
+
+        public List<Questions1> ShowQwestion()
+        {
+            List<Questions1> allquestions;
+            List<Questions1> questions5 = new List<Questions1>();
+            List<Questions1> questions6 = new List<Questions1>();
+            List<Questions1> questions10 = new List<Questions1>();
+            try
+            {
+
+                using (EstyfridmanMathTestEntities db = new EstyfridmanMathTestEntities())
+                {
+                    questions5 = db.Questions1.Include("AmericanAnswers").Where(x => x.Percent == 5).OrderBy(x => Guid.NewGuid()).Take(3).ToList();
+                    questions6 = db.Questions1.Where(x => x.Percent == 6).OrderBy(x => Guid.NewGuid()).Take(4).ToList();
+                    questions10 = db.Questions1.Where(x => x.Percent == 10).OrderBy(x => Guid.NewGuid()).Take(5).ToList();
+
+                }
+                questions5.AddRange(questions10);
+                questions5.AddRange(questions6);
+                allquestions = questions5;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return allquestions;
+
+        }
+        public void updteMarkTable(User user, int mark)
+        {
+            Marks marks = new Marks();
+            marks.Mark = mark;
+            marks.UserId = user.Id;
+            marks.DateTest = DateTime.Now;
+            try
+            {
+
+                using (EstyfridmanMathTestEntities db = new EstyfridmanMathTestEntities())
+                {
+                    db.Marks.Add(marks);
+                    db.SaveChanges();
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL;
+namespace project
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            User user = null;
+            int j = 0;
+            while (user == null)
+            {
+                j++;
+                Console.WriteLine("enter name");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("enter password");
+                string password = Console.ReadLine();
+
+                user = class1.Login(name, password);
+                if (j > 1 && user == null || user == null)
+                {
+                    Console.WriteLine(" פרטיך שגויים");
+
+                }
+            }
+            Console.WriteLine("conected sucssefully");
+            List<Questions1> listQ = class1.ShowQwestion();
+            int mark = 0;
+            int num = 0;
+
+            foreach (Questions1 q in listQ)
+            {
+                Console.WriteLine(q.Question);
+                if (q.IsAmerican)
+                {
+                    List<AmericanAnswers> americanAnswers = q.AmericanAnswers.OrderBy(x => Guid.NewGuid()).ToList();
+                    for (int i = 0; i < americanAnswers.Count; i++)
+                    {
+                        Console.WriteLine((i + 1) + "." + americanAnswers[i].Answers);
+
+                    }
+                    Console.WriteLine("בחר תשובה");
+                    string numString = Console.ReadLine();
+                    int.TryParse(numString, out num);
+                    while (num > americanAnswers.Count)
+                    {
+                        Console.WriteLine(" הבחירה אינה קיימת בחר תשובה");
+                        numString = Console.ReadLine();
+                        int.TryParse(numString, out num);
+                    }
+                    if (americanAnswers[num - 1].IsTrue)
+                        mark += q.Percent;
+
+                }
+                if (!q.IsAmerican)
+                {
+
+
+                    Console.WriteLine(" הכנס תשובה נכונה");
+                    string numString = Console.ReadLine();
+
+                    int.TryParse(numString, out num);
+
+                    mark += q.Percent;
+                }
+            }
+            Console.WriteLine(mark);
+            try
+            {
+                class1.updteMarkTable(user, mark);
+            }
+            catch
+            {
+                Console.WriteLine("לצערנו הפרטים לא נשמרו");
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
